e8f0a9d1a97d5c9804eddc562381f5c9433a381c
Henry Golding <hegold@users.noreply.github.com>
2022-05-20 15:22:16 -0700

    Projectile tests

---
+++ Assets/Tests/ProjectileTests.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor;
+using Unity.FPS.Game;
+
+public class ProjectileTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void ProjectileTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest allows `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    // A test with the [RequiresPlayMode] tag ensures that the test is always run inside PlayMode.
+    [UnityTest]
+    [RequiresPlayMode]
+    public IEnumerator NewTestScriptInPlayModeWithEnumeratorPasses()
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/FPS/Prefabs/Projectiles/Projectile_Blaster.prefab");
+        var obj = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        var projectile = obj.GetComponent<ProjectileBase>();
+
+        projectile.Shoot(null, Vector3.right, 1.0f);
+
+        Assert.IsNotNull(obj);
+
+        // TODO: Extension method for Vec3 compare
+        //Assert.AreEqual(Vector3.right, obj);
+
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
