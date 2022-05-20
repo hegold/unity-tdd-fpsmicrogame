0ecefd10dd02831f34abe6f214052e23ad19e632
Henry Golding <hegold@users.noreply.github.com>
2022-05-20 16:11:16 -0700

    First test load scene and press start then wait for scene load

--- Assets/FPS/Editor/Tests/NewAltUnityTest.cs
+++ Assets/FPS/Editor/Tests/NewAltUnityTest.cs
@@ -3,25 +3,27 @@ using Altom.AltUnityDriver;
 
 public class NewAltUnityTest
 {
-    public AltUnityDriver altUnityDriver;
+    public AltUnityDriver driver;
     //Before any test it connects with the socket
     [OneTimeSetUp]
     public void SetUp()
     {
-        altUnityDriver =new AltUnityDriver();
+        driver =new AltUnityDriver();
     }
 
     //At the end of the test closes the connection with the socket
     [OneTimeTearDown]
     public void TearDown()
     {
-        altUnityDriver.Stop();
+        driver.Stop();
     }
 
     [Test]
-    public void Test()
+    public void TestStartButton()
     {
-	//Here you can write the test
+        driver.LoadScene("IntroMenu");
+        driver.FindObject(By.NAME, "StartButton").Click();
+        driver.WaitForCurrentSceneToBe("MainScene");
     }
 
 }
